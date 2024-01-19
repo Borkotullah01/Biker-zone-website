fd972b1709b062adcb44be526daa6a2cf6c76559
Borkotullah <ak.borkotullah@gmail.com>
2024-01-20 00:44:59 +0600

    biker zone website header nav menu and header carousel slider added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      colors: {
+        'bike-primary': '#E76F51',
+        
+      }
+    }
+  };
